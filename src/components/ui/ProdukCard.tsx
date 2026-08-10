69e9716c4af9a95f90dd4743ea4e0d40d7e96007
bajiff <bagusajifernando@gmail.com>
2026-08-11 01:04:00 +0700

    lanjut ke 🟩 FASE 1 — Sesi 14-15: PostgreSQL + Prisma Dasar

--- src/components/ui/ProdukCard.tsx
+++ src/components/ui/ProdukCard.tsx
@@ -12,12 +12,13 @@ const ProdukCard = ({ nama, harga }: ProdukCardProps) => {
   const addCart = () => setJumlah(jumlah + 1);
   
   return (
-      <section>
-        <h3>{nama}</h3>
-        <p>Rp {harga}</p>
+      <section className="rounded-2xl shadow-2xl border-2 flex flex-col gap-y-1 p-2">
+        <h3 className="text-lg font-bold">{nama}</h3>
+        <p className="text-gray-600">Rp {harga}</p>
         <p>Di keranjang: {jumlah}</p>
-        <button onClick={addCart}>Tambah Keranjang</button>
-      </section>
+        <button className="bg-green-600 text-white rounded px-2 hover:bg-green-800 duration-200 transition"
+        onClick={addCart}>Tambah Keranjang</button>
+      </section> 
   )
 }
 
