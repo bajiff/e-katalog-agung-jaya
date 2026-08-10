428d2dbc8f2166ba25450532d74505e5faeda783
bajiff <bagusajifernando@gmail.com>
2026-08-10 20:28:10 +0700

    done lanjut ke ->📋 RAPOR EVALUASI — Fase 0 (Hari 1 - Hari 10)

--- src/components/ui/KatalogBumdes.tsx
+++ src/components/ui/KatalogBumdes.tsx
@@ -1,32 +1,12 @@
 // src/components/ui/KatalogBumdes.jsx
-// import {daftarProduk} from "../../data/index";
+
 import { ProdukCard } from './index';
-import { useState, useEffect } from 'react';
 import { ProdukBumdes } from '../../App';
 const KatalogBumdes = ({dataProduk} : {dataProduk: ProdukBumdes[]}) => {
-  const [produk, setProduk] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const getData = async () => {
-      const fetchAPI = await fetch("https://fakestoreapi.com/products?limit=5")
-      const response = await fetchAPI.json()
-      setProduk(response);
-      setIsLoading(false);
-    }
-
-    getData()
-  }, []);
   return (
-    <>
-    {isLoading ? (
-      <p>Sedang memuat data dari gudang...</p>
-    ) : (
-      produk.map(item => (
-        <ProdukCard key={item.id} harga={item.price} nama={item.title} />
+      dataProduk.map(item => (
+        <ProdukCard key={item.id} harga={item.harga} nama={item.nama} />
       ))
-    )}
-    </>
   )
 }
 
