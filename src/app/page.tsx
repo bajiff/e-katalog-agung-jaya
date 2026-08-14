443e9eb257df036ab8fa20d649cbdc16c8bf7ae4
bajiff <bagusajifernando@gmail.com>
2026-08-14 23:38:47 +0700

    lanjut udah di bagian pembuatan lib/db.ts udah bikin module udah generate tinggal di selesai in bagian kode 404 nya aja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 // src/app/page.tsx
+import { prisma } from '../../lib/prisma';
 const Home = async () => {  
-
-
+  const produks = await prisma.produk.findMany();
   return (
     <main>
-    <h1>Berjalan</h1>
+      {produks.map(item => (
+        <section key={item.id}>
+          <h2>{item.nama}</h2>
+          <p>{item.deskripsi}</p>
+          <p>Price: ${item.harga.toFixed(2)}</p>
+          <p>Stok: {item.stok}</p>
+          <p>Kategori: {item.kategori}</p>
+        </section>
+      ))}
     </main>
   )
 }
