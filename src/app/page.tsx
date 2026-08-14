3e05c53797fcee384823f565343d2a76fd5f3a22
bajiff <bagusajifernando@gmail.com>
2026-08-14 13:19:04 +0700

    tinggal liat yutub ajalah tutorial masang prisma di nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
 // src/app/page.tsx
+const Home = async () => {  
 
-import { PrismaClient } from "@/prisma/client";
 
-
-const getData = async () => {
-  const prisma = new PrismaClient();
-  const produkBumdes = await prisma.produk.findMany();
-  
   return (
     <main>
-      {produkBumdes.map(item => (
-        <section key={item.id}>
-          <h1>{item.nama}</h1>
-          <p>{item.harga}</p>
-          <p>{item.kategori}</p>
-        </section>
-      ))}
+    <h1>Berjalan</h1>
     </main>
   )
 }
 
-export default getData;
+export default Home;
