ef7c5b022a830202cf87f9f0d717bcf31cd6fc9e
bajiff <bagusajifernando@gmail.com>
2026-08-15 23:01:01 +0700

    Perubahan dari neon ke supabase dan masih menggunakan prisma untuk orm nya atau object relational mapping

--- src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-// src/components/ui/index.ts
-import ProdukCard from "./ProdukCard";
-import KatalogBumdes from "./KatalogBumdes";
-import FormTambahProduk from "./FormTambahProduk";
-
-export {KatalogBumdes, ProdukCard, FormTambahProduk};
